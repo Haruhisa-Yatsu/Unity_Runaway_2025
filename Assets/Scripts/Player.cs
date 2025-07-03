0455e38aa63575cf6bb12a710501070b8e88f128
HaruhisaYatsu <yatsu.haruhisa@nsg.gr.jp>
2025-07-03 11:09:25 +0900

    プレイヤーの移動処理

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    /// <summary>
+    /// ˆÚ“®‚Ì‘¬‚³
+    /// </summary>
+    [SerializeField]
+    private float _velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ‰œˆÚ“®
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * _velocity * Time.deltaTime;
+        }
+        // ¶ˆÚ“®
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= Vector3.right * _velocity * Time.deltaTime;
+        }
+        // è‘OˆÚ“®
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= Vector3.forward * _velocity * Time.deltaTime;
+        }
+        // ‰EˆÚ“®
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * _velocity * Time.deltaTime;
+        }
+    }
+}
