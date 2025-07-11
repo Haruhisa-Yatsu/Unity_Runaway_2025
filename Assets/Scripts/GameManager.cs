52d3f501898f086ff4132779c0b910a9ee5c5b84
HaruhisaYatsu <yatsu.haruhisa@nsg.gr.jp>
2025-07-11 10:02:18 +0900

    ゲーム画面にコインのカウンタを表示

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
+// TextMeshProを扱う際に必要
+using TMPro;
+
+
 public class GameManager: MonoBehaviour
 {
+    [SerializeField]
+    private TMP_Text _text;
+
+    /// <summary>
+    /// 開始時に存在するアイテムの数
+    /// </summary>
+    private int _startItemCount;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _startItemCount = transform.childCount;
     }
 
     // Update is called once per frame
     void Update()
     {
+        _text.text = $" {_startItemCount - transform.childCount} / {_startItemCount}";
+
         //子オブジェクトが０個だった場合
         if(transform.childCount == 0)
         {
